c1f8607954208edcd4db765c722c6e422ce1e996
Jose Soto <jsoto7087@gmail.com>
2024-05-04 12:25:49 -0400

    updates all files

--- server/app.py
+++ server/app.py
@@ -20,6 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+
 migrate = Migrate(app, db)
 db.init_app(app)
 
