fdd8a7a677e96a99f1b0d4b8439bb4683dc27459
Jose Soto <jsoto7087@gmail.com>
2024-05-05 15:06:53 -0400

    adds cars componenet

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import BirdPage from "./BirdPage";
 import BirdDetail from "./BirdDetail"
+import CarsList from "./CarsList";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
               <Route path="/:id" element={ <BirdDetail /> }/>
               <Route path="/" element={ <BirdPage /> }/>
+              <Route path="/cars" element={ <CarsList /> }/>
             </Routes>
           </main>
         </>
---
+++ client/src/components/CarsList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CarsList() {
+
+  return (
+    <div>
+      <h1>Hello from the car List Component</h1>
+    </div>
+  )
+}
