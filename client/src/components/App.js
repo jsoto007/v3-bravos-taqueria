c7bae716c3530221784071b5a1a32c624fe0533c
jsoto007 <jsoto7087@gmail.com>
2025-05-07 22:51:14 -0400

    working with no errors

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import BirdPage from "./BirdPage";
 import BirdDetail from "./BirdDetail"
 import CarsList from "./CarsList";
+import LoginForm from "./LoginForm";
 
 function App() {
 
@@ -12,8 +13,9 @@ function App() {
           <main>
             <Header />
             <Routes>
-              <Route path="/:id" element={ <BirdDetail /> }/>
               <Route path="/" element={ <BirdPage /> }/>
+              <Route path="/:id" element={ <BirdDetail /> }/>
+              <Route path="/login_user" element={ <LoginForm /> }/>
               <Route path="/cars" element={ <CarsList /> }/>
             </Routes>
           </main>
