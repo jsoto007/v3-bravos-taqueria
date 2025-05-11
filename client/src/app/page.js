19b943caa38ea8937b291d68347a28dde3c245ef
jsoto007 <jsoto7087@gmail.com>
2025-05-10 22:54:04 -0400

    fixed deployed error

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from "react"
-import Birds from "./components/birds"
+import Birds from "./components/Birds"
 
 export default function Home() {
 
