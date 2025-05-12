79f470c9e8611ad34f706aac33c85d3eb05636c3
jsoto007 <jsoto7087@gmail.com>
2025-05-12 19:44:18 -0400

    ran revision

--- server/config.py
+++ server/config.py
@@ -19,6 +19,6 @@ app.json.compact = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
