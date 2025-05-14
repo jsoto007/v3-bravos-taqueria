1a80c72a9d5f9c877148b0d91e6ac077b4f03354
jsoto007 <jsoto7087@gmail.com>
2025-05-14 15:11:03 -0400

    nav shows on login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import ProtactedRoutes from './utils/ProtectedRoutes';
 import Auth from './components/Auth'
 import Dashboard from './components/Dashboard'
 import NavBar from './components/NavBar';
+import { UserContext } from './context/UserContextProvider';
 
 
 function App() {
+  const { currentUser } = useContext(UserContext)
  
 
   return (
     <Router>
       <div>
-        <NavBar />
+        {/* <NavBar /> */}
+        {currentUser? <NavBar /> : null}
         <Routes>
           <Route path="/auth" element={<Auth />} />
 
