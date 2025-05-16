790d125ee7567215e9bb5f73524009d086c645c9
jsoto007 <jsoto7087@gmail.com>
2025-05-16 09:51:39 -0400

    working on master cart

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
-import PhotoGallery from "../shared/PhotoGellery";
+
 
 export default function Auth() {
     const [isLogin, setIsLogin] = useState(true);
 
     return (
         <>
-            <PhotoGallery />
+            {isLogin ? <Login /> : <Signup />}
+            <button onClick={() => setIsLogin(!isLogin)}>
+                {isLogin ? "Create an Account" : "Log In"}
+            </button>
         </>
-        // <>
-        //     {isLogin ? <Login /> : <Signup />}
-        //     <button onClick={() => setIsLogin(!isLogin)}>
-        //         {isLogin ? "Create an Account" : "Log In"}
-        //     </button>
-        // </>
     );
 }
--- client/src/components/MasterCarContainer.jsx
+++ client/src/components/MasterCarContainer.jsx
@@ -1,15 +1,15 @@
-
+import PhotoGallery from "../shared/PhotoGellery"
 
 export default function MasterCarContainer() {
     return (
       <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
         <div className="px-4 py-5 sm:px-6">
-          {/* Content goes here */}
-          {/* We use less vertical padding on card headers on desktop than on body sections */}
+          <h1>BMW 2077   |   VIN 23423425lk2345K</h1>
+          <PhotoGallery />
         </div>
-        <div className="px-4 py-5 sm:p-6">{/* Content goes here */}</div>
+        <div className="px-4 py-5 sm:p-6"><h1>Is this the center</h1></div>
         <div className="px-4 py-4 sm:px-6">
-          {/* Content goes here */}
+          <h1>Is this the button?</h1>
           {/* We use less vertical padding on card footers at all sizes than on headers or body sections */}
         </div>
       </div>
