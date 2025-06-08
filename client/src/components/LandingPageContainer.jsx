a0e5228303c4adafa18e6daa232b8e538c4a102c
jsoto007 <jsoto7087@gmail.com>
2025-06-08 15:37:00 -0400

    adds pricing

--- client/src/components/LandingPageContainer.jsx
+++ client/src/components/LandingPageContainer.jsx
@@ -1,10 +1,12 @@
 import LandingPageHero from "./LandingPageHero"
+import LandingPagePricing from "./LandingPagePricing"
 
 export default function LandingPageContainer() {
 
     return (
         <>  
             <LandingPageHero />
+            <LandingPagePricing />
         </>
     )
 }
