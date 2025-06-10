cbcc3923e5cf7cdc44d7eb5cb3515313ef6cedc8
jsoto007 <jsoto7087@gmail.com>
2025-06-10 10:18:24 -0400

    adds links to footer

--- client/src/components/LandingPageContainer.jsx
+++ client/src/components/LandingPageContainer.jsx
@@ -1,5 +1,6 @@
 import LandingPageHero from "./LandingPageHero"
 import LandingPagePricing from "./LandingPagePricing"
+import Footer from "./Footer"
 
 export default function LandingPageContainer() {
 
@@ -7,6 +8,7 @@ export default function LandingPageContainer() {
         <>  
             <LandingPageHero />
             <LandingPagePricing />
+            <Footer />
         </>
     )
 }
