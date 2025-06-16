3f0125357be25f39bece111fb07b54800aa48393
jsoto007 <jsoto7087@gmail.com>
2025-06-16 18:46:43 -0400

    Client: working on displaying photos

--- client/src/utils/PhotoUploader.jsx
+++ client/src/utils/PhotoUploader.jsx
@@ -14,7 +14,6 @@ export default function PhotoUploader({ carInventoryId, open, setOpen }) {
       return;
     }
 
-    console.log("from uploader", carInventoryId)
     
     const uploadedUrls = [];
     try {
