5745f72452ff368ab86f73762503d1a6fca59155
jsoto007 <jsoto7087@gmail.com>
2025-07-18 12:47:00 -0400

    client: fix class error for loading

--- client/src/shared/Loading.jsx
+++ client/src/shared/Loading.jsx
@@ -1,12 +1,12 @@
 export default function Loading() {
 
     return (
-        <div class="flex-col gap-4 w-full flex items-center justify-centerv mt-20">
+        <div className="flex-col gap-4 w-full flex items-center justify-centerv mt-20">
             <div
-                class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
             >
                 <div
-                    class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+                    className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
                 ></div>
             </div>
         </div>
