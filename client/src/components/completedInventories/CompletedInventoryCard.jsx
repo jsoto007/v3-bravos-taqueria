dc7c824bd2a11afb4a64704b59ac21954584f493
jsoto007 <jsoto7087@gmail.com>
2025-07-18 18:44:43 -0400

    updates table

--- client/src/components/completedInventories/CompletedInventoryCard.jsx
+++ client/src/components/completedInventories/CompletedInventoryCard.jsx
@@ -2,28 +2,35 @@ export default function CompletedInventoryCard() {
     return (
       <div className="w-full bg-gray-100 dark:bg-gray-800 outline outline-white/15 lg:rounded-tl-[2rem] rounded-t-lg rounded-b-xl overflow-hidden">
         <div className="overflow-x-auto">
-          <table className="min-w-full text-left divide-y divide-gray-200 dark:divide-gray-700">
+          <table className="min-w-full text-left divide-y-6 divide-gray-400 dark:divide-gray-700">
             <thead>
               <tr>
+                <th className="px-4 py-2">VIN</th>
+                <th className="px-4 py-2">Location</th>
+                <th className="px-4 py-2">User</th>
                 <th className="px-4 py-2">Date</th>
-                <th className="px-4 py-2">Accounted</th>
-                <th className="px-4 py-2">Unaccounted</th>
-                <th className="px-4 py-2">Reviewed</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="divide-y-2 divide-gray-200/30">
               <tr>
-                <td className="px-4 py-2">Alfreds Futterkiste</td>
-                <td className="px-4 py-2">Maria Anders</td>
-                <td className="px-4 py-2">Germany</td>
-                <td className="px-4 py-2">Germany</td>
+                <td className="px-4 py-2">1FAHP2F87HG171820</td>
+                <td className="px-4 py-2">142 Audubon</td>
+                <td className="px-4 py-2">James007</td>
+                <td className="px-4 py-2">12.26.25</td>
               </tr>
               <tr>
-                <td className="px-4 py-2">Centro comercial Moctezuma</td>
-                <td className="px-4 py-2">Francisco Chang</td>
-                <td className="px-4 py-2">Mexico</td>
-                <td className="px-4 py-2">Boolean</td>
+                <td className="px-4 py-2">1FAHP2F87HG171820</td>
+                <td className="px-4 py-2">142 Audubon</td>
+                <td className="px-4 py-2">James007</td>
+                <td className="px-4 py-2">12.26.25</td>
               </tr>
+              <tr>
+                <td className="px-4 py-2">1FAHP2F87HG171820</td>
+                <td className="px-4 py-2">142 Audubon</td>
+                <td className="px-4 py-2">James007</td>
+                <td className="px-4 py-2">12.26.25</td>
+              </tr>
+
             </tbody>
           </table>
         </div>
