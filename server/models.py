f4406e3c00f147a78875b9ac66522a3f1a1cd515
jsoto007 <jsoto7087@gmail.com>
2025-07-29 23:36:18 -0400

    Server: adds full_name to models

--- server/models.py
+++ server/models.py
@@ -45,6 +45,8 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
 
+    full_name = db.Column(db.String, unique=False, nullable=True)
+
     admin = db.Column(db.Boolean, default=False)
     is_owner_admin = db.Column(db.Boolean, default=False)
     account_group_id = db.Column(db.Integer, db.ForeignKey('account_groups.id'), nullable=False)
