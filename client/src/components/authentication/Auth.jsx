8b828f2a8a81ad075f6d0838d4c5d56c73b476a1
jsoto007 <jsoto7087@gmail.com>
2025-07-31 09:42:54 -0400

    updates UI

--- client/src/components/authentication/Auth.jsx
+++ client/src/components/authentication/Auth.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import Login from "./Login";
 import Signup from "./Signup";
+import ActionBtn from "../../shared/ActionBtn";
 
 
 export default function Auth() {
     const [isLogin, setIsLogin] = useState(true);
 
-    // removed the avility to create an account for the user. Thiking that it should be an admin creating the accounts. Dont think that this would scare well. This is a future me problem now.
-
     return (
         <>
            <h1 className="text-4xl font-bold text-center text-gray-900 dark:text-white my-8">Auto Tracker</h1>
             {isLogin ? <Login /> : <Signup />}
-            <button onClick={() => setIsLogin(!isLogin)}>
+            <hr className="my-15"/>
+            <ActionBtn label="Create Account" onClick={() => setIsLogin(!isLogin)}>
                 {isLogin ? "Create an Account" : "Log In"}
-            </button>
+            </ActionBtn>
         </>
     );
 }
