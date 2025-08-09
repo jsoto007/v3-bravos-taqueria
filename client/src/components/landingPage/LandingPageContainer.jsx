d28e592ae945beca03089b61e4dc18d36a257280
jsoto007 <jsoto7087@gmail.com>
2025-08-08 20:27:13 -0400

    udates navon user?

--- client/src/components/landingPage/LandingPageContainer.jsx
+++ client/src/components/landingPage/LandingPageContainer.jsx
@@ -1,14 +1,12 @@
 import LandingPageHero from "./LandingPageHero"
 import LandingPagePricing from "./LandingPagePricing"
-import Footer from "../../shared/Footer"
 import LandingPageLogos from "./LandingPageLogos"
-import LandingPageNav from "./LandingPageNav"
+
 
 export default function LandingPageContainer() {
 
     return (
         <>  
-            <LandingPageNav />
             <LandingPageHero />
             <LandingPageLogos />
             <LandingPagePricing />
