c4d74374d56a760ce95800f216add128de0b2976
jsoto007 <jsoto7087@gmail.com>
2025-09-14 18:11:01 -0400

    updates UI and logic

--- client/src/components/completedInventories/CompletedInventories.jsx
+++ client/src/components/completedInventories/CompletedInventories.jsx
@@ -1,33 +1,8 @@
-import React, { useEffect, useState, useContext } from "react";
-import { UserContext } from '../../context/UserContextProvider';
+import React from "react";
+
 import CompletedInventoryCard from './CompletedInventoryCard';
-import { useNavigate } from 'react-router-dom';
-import Loading from "../../shared/Loading";
 
 export default function CompletedInventories() {
-    const [inventories, setInventories] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const { currentUser } = useContext(UserContext);
-    const navigate = useNavigate();
-    const isAdmin = currentUser?.admin === true;
-
-    useEffect(() => {
-        if (!currentUser?.id) return;
-    
-        fetch(`/api/user_inventories/history/${currentUser.id}`)
-            .then(res => res.json())
-            .then(data => {
-                setInventories(data);
-                setLoading(false);
-            })
-            .catch(err => {
-                setLoading(false);
-            });
-    }, [currentUser]);
-
-    if (loading) {
-        return <Loading />;
-    }
 
     return (
         <>
