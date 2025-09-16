6f41bd5caedc4f6f8916b40f4fee059cd69ad4a3
jsoto007 <jsoto7087@gmail.com>
2025-09-16 16:17:15 -0400

    Client: adds fade in to dashboard

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,41 +3,45 @@ import InventoryFeed from "./inventory/InventoryFeed"
 import InventoryFilesDownloadCard from "./inventory/InventoryFilesDownloadCard"
 import InventoryOverviewCard from "./inventory/InventoryOverviewCard"
 import AddCarBtn from "../shared/AddCarBtn"
+import FadeIn from "../shared/FadeIn"
 
 
 export default function Dashboard() {
   return (
-    <div className="bg-neutral-50 dark:bg-gray-900/30 text-gray-900 dark:text-white mt-10 rounded-xl pt-4">
-      <div className="mx-auto">
-        <p className="mt-2 flex justify-start text-pretty text-4xl font-serif font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
-          Dashboard
-        </p>
-        <div className="flex justify-end mt-4">
-          <AddCarBtn />
-        </div>
-        <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-          <div className="flex p-px lg:col-span-4">
-            <CompletedInventoryCard />
-         
+    <FadeIn>
+      <div className="bg-neutral-50 dark:bg-gray-900/30 text-gray-900 dark:text-white mt-10 rounded-xl pt-4">
+
+        <div className="mx-auto">
+          <p className="mt-2 flex justify-start text-pretty text-4xl font-serif font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
+            Dashboard
+          </p>
+          <div className="flex justify-end mt-4">
+            <AddCarBtn />
           </div>
-          <div className="flex p-px lg:col-span-2">
-            <div className="w-full overflow-hidden rounded-lg bg-white dark:bg-gray-800 outline outline-white/15 lg:rounded-tr-[2rem]">
-            <InventoryFeed />
-         
+          <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+            <div className="flex p-px lg:col-span-4">
+              <CompletedInventoryCard />
+          
             </div>
-          </div>
-          <div className="flex p-px lg:col-span-2">
-            {/* <div className="w-full overflow-hidden rounded-lg bg-white dark:bg-gray-800 outline outline-white/15 lg:rounded-bl-[2rem]">
-            <InventoryOverviewCard />
-            </div> */}
-          </div>
-          <div className="flex p-px lg:col-span-4">
-            {/* <div className="w-full overflow-hidden rounded-lg bg-gray-100 dark:bg-gray-800 outline outline-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
-              <InventoryFilesDownloadCard />
-            </div> */}
+            <div className="flex p-px lg:col-span-2">
+              <div className="w-full overflow-hidden rounded-lg bg-white dark:bg-gray-800 outline outline-white/15 lg:rounded-tr-[2rem]">
+              <InventoryFeed />
+          
+              </div>
+            </div>
+            <div className="flex p-px lg:col-span-2">
+              {/* <div className="w-full overflow-hidden rounded-lg bg-white dark:bg-gray-800 outline outline-white/15 lg:rounded-bl-[2rem]">
+              <InventoryOverviewCard />
+              </div> */}
+            </div>
+            <div className="flex p-px lg:col-span-4">
+              {/* <div className="w-full overflow-hidden rounded-lg bg-gray-100 dark:bg-gray-800 outline outline-white/15 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]">
+                <InventoryFilesDownloadCard />
+                </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </FadeIn>
   )
 }
