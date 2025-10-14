37a50e86a13480bcb657473cd4b0d304cc88cbcc
jsoto007 <jsoto7087@gmail.com>
2025-10-14 00:20:27 -0400

    updates footer

--- client/src/shared/Footer.jsx
+++ client/src/shared/Footer.jsx
@@ -91,7 +91,7 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-10 text-center text-sm/6 text-gray-600 dark:text-gray-400">
-            &copy; {year} <a href="https://sotodev.com/" target="_blank" rel="noopener noreferrer">SotoDev</a>, Inc. All rights reserved.
+            &copy; {year} <a href="https://sotodev.com/" target="_blank" rel="noopener noreferrer">SotoDev</a>, LLC. All rights reserved.
         </p>
       </div>
     </footer>
